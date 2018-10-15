b38eb47f9271aad3bb9b388a81f8cc72740292f0
bassyouni <omae.bassyouni991@gmail.com>
2018-10-15 15:22:50 +0200

    Initial Commit

---
+++ SoleeklabTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoleeklabTask
+//
+//  Created by MacBook Pro on 10/15/18.
+//  Copyright © 2018 Bassyouni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
