57df2ca6e79f9fe0d32a4bf965f07a380a8f5d5c
bassyouni <omae.bassyouni991@gmail.com>
2018-10-29 21:26:26 +0200

    new line added

--- SoleeklabTask/Controller/LoginVC.swift
+++ SoleeklabTask/Controller/LoginVC.swift
@@ -21,7 +21,7 @@ class LoginVC: ParentViewController  {
     //MARK:- view methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // sex dah walla eh, walla da merge conflict?
         emailTextField.delegate = self
         passwordTextField.delegate = self
         emailTextField.tag = 1
